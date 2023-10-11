c0e6fd57e9cee7fe44c7cc5f54971a028abe3f13
GolaJakub <kuba300901@gmail.com>
2023-10-12 00:11:18 +0200

    Readme and documentation

---
+++ README.md
@@ -0,0 +1,19 @@
+## Gameplay
+- This is extended TicTacToe Game that has 5x5 board.
+
+- We can use command `````'show moves'````` to display available moves.
+
+- To move we should choose one move from list by typing 'move #number' ex 'move #1'
+
+- To win player has to has 5 marks in row, column or diagonally.
+
+```Created by: Jakub Gola & Bartosz Laskowski```
+
+## Configuration:
+1) ```git clone https://github.com/Nougat00/TicTacToe5x5.git```
+2) ```pip install easyAI```
+3) ```python main.py ```
+
+
+
+
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
 
-class TicTacToe4x4(TwoPlayerGame):
+
+"""This is extended TicTacToe Game that has 5x5 board.
+We can use command 'show moves' to display available moves.
+To move we should choose one move from list by typing 'move #number' ex 'move #1'
+To win player has to has 5 marks in row, column or diagonally.
+
+Created by: Jakub Gola & Bartosz Laskowski
+"""
+class TicTacToe5x5(TwoPlayerGame):
     def __init__(self, players):
+        """Initializes the Tic-Tac-Toe game with a 5x5 board and the specified players.
+
+            Parameters:
+            players (list): Players List (np. [Human_Player(), AI_Player()]).
+        """
         self.players = players
         self.board = [[0 for _ in range(5)] for _ in range(5)]
         self.current_player = 1
 
     def possible_moves(self):
+        # Returns a list of available moves as tuples (row, column).
         moves = []
         for i in range(5):
             for j in range(5):
@@ -15,20 +29,30 @@ class TicTacToe4x4(TwoPlayerGame):
         return moves
 
     def make_move(self, move):
+        """Makes a move on the board based on the specified move (row, column).
+
+        Parameters:
+        move (tuple): Ruch w formie krotki (row, column).
+        """
         i, j = move
         self.board[i][j] = self.current_player
 
     def lose(self):
-        # Sprawdzamy, czy któryś z graczy wygrał w poziomie, pionie lub na przekątnych
+        """Checks if the current player has lost the game.
+
+        Returns:
+        bool: True if player lost, otherwise False
+        """
+        # We check whether one of the players won horizontally, vertically or diagonally
         for i in range(4):
-            # Sprawdzanie wierszy
+            # Checking rows
             if self.board[i][0] == self.board[i][1] == self.board[i][2] == self.board[i][3] == self.board[i][4] != 0:
                 return True
-            # Sprawdzanie kolumn
+            # Checking columns
             if self.board[0][i] == self.board[1][i] == self.board[2][i] == self.board[3][i] == self.board[i][4] != 0:
                 return True
 
-        # Sprawdzanie przekątnych
+        # Checking diagonals
         if self.board[0][0] == self.board[1][1] == self.board[2][2] == self.board[3][3] == self.board[4][4] != 0:
             return True
 
@@ -38,9 +62,15 @@ class TicTacToe4x4(TwoPlayerGame):
         return False
 
     def is_over(self):
+        """Checks if the game is over, either due to a player winning or no available moves.
+
+        Returns:
+        bool: True if game is over, otherwise False
+        """
         return self.lose() or len(self.possible_moves()) == 0
 
     def show(self):
+        # Displays the current state of the game on the console.
         for i in range(5):
             for j in range(5):
                 if self.board[i][j] == 1:
@@ -53,10 +83,16 @@ class TicTacToe4x4(TwoPlayerGame):
         print()
 
     def scoring(self):
-        return 100 if game.lose() else 0
+        """Calculates the game score.
+
+        Returns:
+            int: Returns -10 if the current player has lost, else returns 10.
+        """
+        return -10 if game.lose() else 10
+
 
 if __name__ == "__main__":
     ai_algo = Negamax(6)
     human = Human_Player()
-    game = TicTacToe4x4([human, AI_Player(ai_algo)])
+    game = TicTacToe5x5([human, AI_Player(ai_algo)])
     game.play()
